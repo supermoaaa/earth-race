0103a92e1815bb3a7dda41c9a9ef4b3bef959dc5
lelapinvolant@hotmail.fr <lelapinvolant@hotmail.fr@39760f87-477c-9817-2185-4c5f5fb91e3d>
2013-04-08 20:57:38 +0000

    ajout du level de graphisme

--- script/confParser.py
+++ script/confParser.py
@@ -221,7 +221,7 @@ def loadConf():
 
 def checkConf():
 	if not hasattr(gl, 'graphic'):
-		gl.graphic = [ True, rd.getAnisotropicFiltering() ]
+		gl.graphic = [ True, rd.getAnisotropicFiltering(), 3 ]
 	if not hasattr(gl, 'sound'):
 		gl.sound = [ 50 ]
 	if not hasattr(gl, 'skin'):

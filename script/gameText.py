9ab29eb41b0feb998b9661bf9d43f4c77d9e6f2b
laurent.castanier100@gmail.com <laurent.castanier100@gmail.com@39760f87-477c-9817-2185-4c5f5fb91e3d>
2013-06-07 17:47:01 +0000

    diverse petite amélioration + ajout du début d'un circuit route goudronner le sol est fini sont dépliage est en cours (pointe a 220 kmh avec MegaRS) ajout d'un dossier pour les sky box

--- script/gameText.py
+++ script/gameText.py
@@ -31,5 +31,5 @@ def write():
 	blf.position(logic.font_id, (width * 0.2), (height * 0.9), 0)
 	blf.size(logic.font_id, 25, 72)
 	bgl.glColor4f(0.2, 0.5, 1, 1) 
-	blf.draw(logic.font_id, "appuier sur espace pour passer l\'intro")
+	blf.draw(logic.font_id, "appuyer sur espace pour passer l\'intro")
 

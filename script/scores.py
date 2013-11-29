11ff143628f43ea22d0202c98c4d3b183f1633c7
lelapinvolant@hotmail.fr <lelapinvolant@hotmail.fr@39760f87-477c-9817-2185-4c5f5fb91e3d>
2013-11-29 18:50:22 +0000

    ajout de la prise en compte du nombre de tours dans le tri des scores

--- script/scores.py
+++ script/scores.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 class Scores:
 	def __init__(self, mapName):
 		self.mapName=mapName;
@@ -16,19 +18,21 @@ class Scores:
 		self.scores[playerName].append(duration)
 		self.scores[playerName].sort()
 		self.scores[playerName]=self.scores[playerName][0:4]
-		self.lastScores.append([playerName, duration])
+		self.lastScores.append([playerName, duration, gl.nbLaps])
 
 	def getLastScores(self):
-		return sorted(self.lastScores, key=lambda x:x[1])
+		return sorted(self.lastScores, key=itemgetter(1))
 
 	def getBestScores(self):
-		orderedScores = sorted(self.scores.items(), key=lambda x: x[1])[:8]
+		orderedScores = self.__sortScores(self.scores.items())[:8]
 		bestScores = []
 		for key, listValues in orderedScores:
 			for value in listValues:
 				bestScores.append([key,value])
-		orderedBestScores = sorted(bestScores, key=lambda x: x[1])
-		return orderedBestScores[:8]
+		return self.__sortScores(bestScores)[:8]
 
 	def getPlayerBestScores(self, playerName):
 		return self.scores.get(playerName, [])
+
+	def __sortScores(self, scores):
+		return sorted(scores, key=lambda x: x[1][0]/x[1][1])

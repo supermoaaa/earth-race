8dc02e1c93f1496ec2241a37ba9fff0f4792a3df
lelapinvolant@hotmail.fr <lelapinvolant@hotmail.fr@39760f87-477c-9817-2185-4c5f5fb91e3d>
2014-03-17 18:29:50 +0000

    améilioration de la lisibilité du code

--- script/writeOnScreen.py
+++ script/writeOnScreen.py
@@ -27,7 +27,7 @@ class bflManager:
 
 	def __new__(cls, *args, **kwargs):
 		if not cls._instance:
-			cls._instance = super(bflManager, cls).__new__(cls, *args, **kwargs)
+			cls._instance = super().__new__(cls, *args, **kwargs)
 		return cls._instance
 
 	def __init__(self):
